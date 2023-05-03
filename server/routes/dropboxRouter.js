6e5ba649aa194eea1308e33736af814483db078b
GhosTHaise <Ghostrex2@gmail.com>
2023-05-03 18:05:16 +0300

    Create connection to dropBox

--- server/routes/dropboxRouter.js
+++ server/routes/dropboxRouter.js
@@ -1,12 +1,19 @@
 import express from "express"
+import * as dotenv from "dotenv"
+import {Dropbox} from "dropbox";
 
-
+dotenv.config();
 const Router = express.Router();
 
+//Create new instance of DropBox
+const dbx = new Dropbox({accessToken : process.env.DROPBOX_ACCESS_TOKEN});
 Router.route("/").get((req,res)=>{
     res.status(200).json({
         message : "Hello from GhosT"
     })
 })
 
+Router.route("/").post((req,res)=>{
+
+})
 export default Router;
