4a86b7f662ad28cb233a6aed6488d8fafa6450ed
GhosTHaise <Ghostrex2@gmail.com>
2023-05-04 20:14:25 +0300

    Create function to get date as formated string

--- server/routes/dropboxRouter.js
+++ server/routes/dropboxRouter.js
@@ -1,19 +1,28 @@
 import express from "express"
 import * as dotenv from "dotenv"
 import {Dropbox} from "dropbox";
-
+import multer from "multer";
+import {Date_Now_String_For_Dir_Name} from "../utils";
 dotenv.config();
+
 const Router = express.Router();
+
+const storage = multer.memoryStorage();
+const Upload = multer({ storage});
 
 //Create new instance of DropBox
 const dbx = new Dropbox({accessToken : process.env.DROPBOX_ACCESS_TOKEN});
+
 Router.route("/").get((req,res)=>{
     res.status(200).json({
         message : "Hello from GhosT"
     })
 })
 
-Router.route("/").post((req,res)=>{
+Router.route("/").post(Upload.single("file_upload"),(req,res)=>{
+    const file = req.file;
 
-})
+    dbx.filesUpload()
+});
+
 export default Router;
---
+++ server/utils/index.js
@@ -0,0 +1,22 @@
+
+
+/** 
+@param {string} _val 
+**/
+const PadZero = (_val) => {
+    return _val.padStart(2,"0");
+}
+
+const Date_Now_String_For_Dir_Name = () => {
+    const date_now = new Date();
+    const day = PadZero(String(date_now.getDate()));
+    const month = PadZero(String(date_now.getMonth() + 1));
+    const year = date_now.getFullYear();
+
+    return `${day}_${month}_${year}`;
+}
+
+
+export {
+    Date_Now_String_For_Dir_Name
+}
