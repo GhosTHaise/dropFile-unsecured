c3b77145cc2756d6fae13907eae27fa5cf96a45b
GhosTHaise <Ghostrex2@gmail.com>
2023-05-24 20:26:54 +0300

    create root to backblaze router

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ import cookieParser from "cookie-parser";
 
 import dropboxRouter from "./routes/dropboxRouter.js";
 import AuthRouter from "./routes/auth.js";
-
+import backblazeRouter from "./routes/BackBlazeRouter.js";
 const app = express();          //1 hour
 const Fourhour = 1000 * 60 * 60 * 4;
 //middleware
@@ -25,5 +25,6 @@ app.use(session({
 
 app.use("/api/v1/dropbox",dropboxRouter);
 app.use("/api/v1/auth",AuthRouter);
+app.use("/api/v1/backblaze",backblazeRouter);
 
 app.listen(8080,()=> console.log("App start on : http://localhost:8080"));
